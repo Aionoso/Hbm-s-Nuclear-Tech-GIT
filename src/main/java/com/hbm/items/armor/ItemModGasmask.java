9161e99efc52e264d87e3d0137b5b10e569afbe6
ApexElite3303 <96636444+ApexElite3303@users.noreply.github.com>
2022-12-26 20:22:55 -0500

    uh tried to add a gasmask filter system
    entirely plagiarized from the 1.7.10 version of src/main/java/com/hbm/util/ArmorUtil.java

--- src/main/java/com/hbm/items/armor/ItemModGasmask.java
+++ src/main/java/com/hbm/items/armor/ItemModGasmask.java
@@ -77,3 +77,153 @@ public class ItemModGasmask extends ItemArmorMod {
 		modelM65.render(event.getEntityPlayer(), 0.0F, 0.0F, 0, yawWrapped, pitch, 0.0625F);
 	}
 }
+
+ /*my own horrific attempt at trying to implement the filter system. i dont know how to program whatsoever.
+ eh, whats the worst that could happen. i cant mess up a nonexistent system.
+
+public static void installGasMaskFilter(ItemStack mask, ItemStack filter) {
+		
+		if(mask == null || filter == null)
+			return;
+		
+		if(!mask.hasTagCompound())
+			mask.stackTagCompound = new NBTTagCompound();
+		
+		NBTTagCompound attach = new NBTTagCompound();
+		filter.writeToNBT(attach);
+		
+		mask.stackTagCompound.setTag(FILTERK_KEY, attach);
+	}
+	
+public static void removeFilter(ItemStack mask) {
+		
+		if(mask == null)
+			return;
+		
+		if(!mask.hasTagCompound())
+			return;
+		
+		mask.stackTagCompound.removeTag(FILTERK_KEY);
+	}
+	
+public static ItemStack getGasMaskFilterRecursively(ItemStack mask, EntityLivingBase entity) {
+		
+		ItemStack filter = getGasMaskFilter(mask);
+		
+		if(filter == null && ArmorModHandler.hasMods(mask)) {
+			
+			ItemStack mods[] = ArmorModHandler.pryMods(mask);
+			
+			if(mods[ArmorModHandler.helmet_only] != null && mods[ArmorModHandler.helmet_only].getItem() instanceof IGasMask)
+				filter = ((IGasMask)mods[ArmorModHandler.helmet_only].getItem()).getFilter(mods[ArmorModHandler.helmet_only], entity);
+		}
+		
+		return filter;
+	}
+public static ItemStack getGasMaskFilter(ItemStack mask) {
+		
+		if(mask == null)
+			return null;
+		
+		if(!mask.hasTagCompound())
+			return null;
+		
+		NBTTagCompound attach = mask.stackTagCompound.getCompoundTag(FILTERK_KEY);
+		ItemStack filter = ItemStack.loadItemStackFromNBT(attach);
+		
+		return filter;
+	}
+public static void damageGasMaskFilter(EntityLivingBase entity, int damage) {
+		
+		ItemStack mask = entity.getEquipmentInSlot(4);
+		
+		if(mask == null)
+			return;
+		
+		if(!(mask.getItem() instanceof IGasMask)) {
+			
+			if(ArmorModHandler.hasMods(mask)) {
+				
+				ItemStack mods[] = ArmorModHandler.pryMods(mask);
+				
+				if(mods[ArmorModHandler.helmet_only] != null && mods[ArmorModHandler.helmet_only].getItem() instanceof IGasMask)
+					mask = mods[ArmorModHandler.helmet_only];
+			}
+		}
+		
+		if(mask != null)
+			damageGasMaskFilter(mask, damage);
+	}
+public static void damageGasMaskFilter(ItemStack mask, int damage) {
+		ItemStack filter = getGasMaskFilter(mask);
+		
+		if(filter == null) {
+			if(ArmorModHandler.hasMods(mask)) {
+				ItemStack mods[] = ArmorModHandler.pryMods(mask);
+				
+				if(mods[ArmorModHandler.helmet_only] != null && mods[ArmorModHandler.helmet_only].getItem() instanceof IGasMask)
+					filter = getGasMaskFilter(mods[ArmorModHandler.helmet_only]);
+			}
+		}
+		
+		if(filter == null || filter.getMaxDamage() == 0)
+			return;
+		
+		filter.setItemDamage(filter.getItemDamage() + damage);
+		
+		if(filter.getItemDamage() > filter.getMaxDamage())
+			removeFilter(mask);
+		else
+			installGasMaskFilter(mask, filter);
+	}
+	
+	public static void addGasMaskTooltip(ItemStack mask, EntityPlayer player, List list, boolean ext) {
+		
+		if(mask == null || !(mask.getItem() instanceof IGasMask))
+			return;
+		
+		ItemStack filter = ((IGasMask)mask.getItem()).getFilter(mask, player);
+		
+		if(filter == null) {
+			list.add(EnumChatFormatting.RED + "No filter installed!");
+			return;
+		}
+		
+		list.add(EnumChatFormatting.GOLD + "Installed filter:");
+		
+		int meta = filter.getItemDamage();
+		int max = filter.getMaxDamage();
+		
+		String append = "";
+		
+		if(max > 0) {
+			append = " (" + ((max - meta) * 100 / max) + "%)";
+		}
+		
+		List<String> lore = new ArrayList();
+		list.add("  " + filter.getDisplayName() + append);
+		filter.getItem().addInformation(filter, player, lore, ext);
+		ForgeEventFactory.onItemTooltip(filter, player, lore, ext);
+		lore.forEach(x -> list.add(EnumChatFormatting.YELLOW + "  " + x));
+	}
+	
+	public static boolean isWearingEmptyMask(EntityPlayer player) {
+		
+		ItemStack mask = player.getEquipmentInSlot(4);
+		
+		if(mask == null)
+			return false;
+		
+		if(mask.getItem() instanceof IGasMask) {
+			return getGasMaskFilter(mask) == null;
+		}
+		
+		ItemStack mod = ArmorModHandler.pryMods(mask)[ArmorModHandler.helmet_only];
+		
+		if(mod != null && mod.getItem() instanceof IGasMask) {
+			return getGasMaskFilter(mod) == null;
+		}
+		
+		return false;
+	}
+*/
