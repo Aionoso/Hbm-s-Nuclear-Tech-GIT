39e456ffa3ea33a54c0878b33c6843cc397bcfde
ElPudgino <140695160+ElPudgino@users.noreply.github.com>
2024-03-30 15:02:24 +0300

    Update ExplosionTom.java
    Now explosion is like in 1.7.10 version except it spawns basalt instead of tektite because tektite was removed for some unknown reason.

--- src/main/java/com/hbm/explosion/ExplosionTom.java
+++ src/main/java/com/hbm/explosion/ExplosionTom.java
@@ -1,11 +1,15 @@
 package com.hbm.explosion;
 
 import com.hbm.config.CompatibilityConfig;
-
+import com.hbm.blocks.ModBlocks;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.BlockAir;
+import net.minecraft.block.material.Material;
 
 public class ExplosionTom
 {
@@ -82,36 +86,84 @@ public class ExplosionTom
 		return this.n > this.nlimit;
 	}
 
-	private void breakColumn(int x, int z)
-	{
+	private void breakColumn(int x, int z) {
 		int dist = this.radius2 - (x * x + z * z);
-		
-		if (dist > 0)
-		{
+
+		if(dist > 0) {
 			int pX = posX + x;
 			int pZ = posZ + z;
-			
+			double X = Math.pow((this.posX - pX), 2);
+			double Z = Math.pow((this.posZ - pZ), 2);
+			double distance = Math.sqrt(X + Z); // Distance calculations used for crater rim stuff
+
 			int y = 256;
-			
+			int terrain = 63;
+
+			double cA = (terrain - Math.pow(Math.E, -Math.pow(Math.sqrt(x * x + z * z), 2) / 40000) * 13) + worldObj.rand.nextInt(2); // Basic crater bowl shape
+			double cB = cA + Math.pow(Math.E, -Math.pow(Math.sqrt(x * x + z * z) - 200, 2) / 400) * 13 ;// Crater peak ring
+			int craterFloor = (int) (cB + Math.pow(Math.E, -Math.pow(Math.sqrt(x * x + z * z) - 500, 2) / 2000) * 37); // Crater rim
 			for(int i = 256; i > 0; i--) {
-				if(worldObj.getBlockState(new BlockPos(pX, i, pZ)).getBlock() != Blocks.AIR) {
+				BlockPos ps = new BlockPos(pX, i, pZ);
+				if(i == craterFloor || !(worldObj.getBlockState(ps).getBlock() instanceof BlockAir)) {
 					y = i;
 					break;
 				}
 			}
-			
-			int height = 70;
-			int offset = 10;
-			//int threshold = height - (int) ((float)dist * (float)height / (float)this.radius2) - 1 + worldObj.rand.nextInt(2);
-			int threshold = (int) ((float)Math.sqrt(x * x + z * z) * (float)(height + offset) / (float)this.radius) + worldObj.rand.nextInt(2) - offset;
-			
+			int height = terrain - 14;
+			int offset = 20;
+			int threshold = (int) ((float) Math.sqrt(x * x + z * z) * (float) (height + offset) / (float) this.radius) + worldObj.rand.nextInt(2) - offset;
+
 			while(y > threshold) {
-				
+
 				if(y == 0)
 					break;
-				
-				worldObj.setBlockToAir(new BlockPos(pX, y, pZ));
-				
+				if(y <= craterFloor) {
+					BlockPos ppp = new BlockPos(pX,y,pZ);
+					if(worldObj.rand.nextInt(499) < 1) {
+						worldObj.setBlockState(ppp, ModBlocks.basalt.getDefaultState());
+					} else {
+						worldObj.setBlockState(ppp, ModBlocks.basalt.getDefaultState());
+					}
+
+				} else {
+					if(y > terrain + 1) {
+						if(distance < 500) // used so that old terrain inside crater rim is destroyed, while rim material "floods" terrain outside.
+						{
+							for(int i = -2; i < 3; i++) {
+								for(int j = -2; j < 3; j++) {
+									for(int k = -2; k < 3; k++) {
+										IBlockState bl = worldObj.getBlockState(new BlockPos(pX + i, y + j, pZ + k));
+										if(bl.getMaterial() == Material.WATER || bl.getMaterial() == Material.ICE || bl.getMaterial() == Material.SNOW || bl.getMaterial().getCanBurn()) {
+											BlockPos ps1 = new BlockPos(pX + i,y,pZ +k);
+											BlockPos ps2 = new BlockPos(pX,y,pZ);
+											worldObj.setBlockToAir(ps1);
+											worldObj.setBlockToAir(ps2);
+										}
+									}
+								}
+							}
+							BlockPos ps3 = new BlockPos(pX,y,pZ);
+							worldObj.setBlockToAir(ps3);
+						}
+					} else {
+						for(int i = -2; i < 3; i++) {
+							for(int j = -2; j < 3; j++) {
+								for(int k = -2; k < 3; k++) {
+									IBlockState bl = worldObj.getBlockState(new BlockPos(pX + i, y + j, pZ + k));
+									if(bl.getMaterial() == Material.WATER || bl.getMaterial() == Material.ICE || bl.getBlock() instanceof BlockAir) {
+										BlockPos ps1 = new BlockPos(pX + i,y,pZ +k);
+										BlockPos ps2 = new BlockPos(pX,y,pZ);
+										worldObj.setBlockState(ps1, Blocks.FLOWING_LAVA.getDefaultState());
+										worldObj.setBlockState(ps2, Blocks.FLOWING_LAVA.getDefaultState());
+									}
+								}
+							}
+						}
+						BlockPos ps3 = new BlockPos(pX,y,pZ);
+						worldObj.setBlockState(ps3, Blocks.FLOWING_LAVA.getDefaultState());
+					}
+
+				}
 				y--;
 			}
 		}
